4a8ad9ffea309d82a41745c4fa9559a1f64364b2
GhosTHaise <ghostrex2@gmail.com>
2025-12-20 21:38:55 +0300

    call sonic 3 model

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from cartesia-ai-small-app!")
+from cartesia import Cartesia
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+client = Cartesia(api_key=os.environ["CARTESIA_API_KEY"])
+
+chunk_iter = client.tts.bytes(
+	model_id="sonic-3",
+	transcript="I can't wait to see what you'll create!",
+	voice={
+			"mode": "id",
+			"id": "6ccbfb76-1fc6-48f7-b71d-91ac6298247b",
+	},
+	output_format={
+			"container": "wav",
+			"sample_rate": 44100,
+			"encoding": "pcm_f32le",
+	},
+)
+
+with open("voices/sonic.wav", "wb") as f:
+	for chunk in chunk_iter:
+		f.write(chunk)
